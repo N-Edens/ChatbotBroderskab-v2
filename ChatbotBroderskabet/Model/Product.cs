cf430639c80c39ab5e0d293cba52ce9ee840e45d
name <Nicolaiedens@gmail.com>
2023-10-30 11:54:19 +0100

    Initial commit

---
+++ ChatbotBroderskabet/Model/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatbotBroderskabet.Client.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name must be less than 50 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+
+        public string Category { get; set; }
+    }
+}
